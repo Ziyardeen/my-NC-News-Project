823b3dd880a770a3a34e75358d2407e7dd1a8870
Ziyardeen Alhassan <zforkah@gmail.com>
2024-04-16 11:17:45 +0100

    Task 4 Completed with Test Suit and endpoint.js Updated

--- app.controller.js
+++ app.controller.js
@@ -1,4 +1,4 @@
-const fetchTopics = require("./app.models");
+const { fetchTopics, fetchArticleById } = require("./app.models");
 const endpoints = require("./endpoints.json");
 
 function healthcheck(req, res, next) {
@@ -18,4 +18,16 @@ function getEndpoints(req, res, next) {
   res.status(200).send(endpoints);
 }
 
-module.exports = { healthcheck, getTopics, getEndpoints };
+function getArticleById(req, res, next) {
+  const article_id = req.params.article_id;
+
+  fetchArticleById(article_id)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+module.exports = { healthcheck, getTopics, getEndpoints, getArticleById };
