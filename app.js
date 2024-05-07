695acd2c213b51f8e46ba96640d265dbfeaa86d2
Ziyardeen Alhassan <zforkah@gmail.com>
2024-05-07 16:20:59 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -14,15 +14,9 @@ const {
 const cors = require("cors");
 
 const app = express();
-
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-
 app.use(express.json());
+
+app.use(cors());
 
 // SERVER HEALTHCHECK
 app.get("/api/healthcheck", healthcheck);
