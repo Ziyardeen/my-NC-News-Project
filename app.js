561c57a503cda209ba52df68892deabc4ae61a9d
Ziyardeen Alhassan <zforkah@gmail.com>
2024-05-07 16:03:25 +0100

    cors solution

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ const {
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // SERVER HEALTHCHECK
